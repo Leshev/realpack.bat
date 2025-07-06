01624c8d61e8d1f902d6473f38e93bb5c578bf28
Leonid <90864230+Leshev@users.noreply.github.com>
2025-07-06 12:46:47 +0500

    Update realpack.bat
    Легкий fix

--- realpack.bat
+++ realpack.bat
@@ -5,7 +5,14 @@ title Install plugin realpack
 mode con:cols=100 lines=11
 rmdir /S /q realpack
 md realpack
-:start
+cls
+echo Download realpack in GitHub by Leshev
 powershell -Command "Invoke-WebRequest https://github.com/Leshev/realpack/raw/refs/heads/main/realpack.zip -OutFile realpack\package.zip"
+cls
+echo Instal files
 powershell -command "Expand-Archive -Force '%~dp0realpack\package.zip' '%~dp0realpack\'"
 del realpack\package.zip
+cls
+echo Complited!
+timeout /t 2 >nul
+exit
