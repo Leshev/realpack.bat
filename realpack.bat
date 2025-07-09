03c04260278393f6e0ee7aa0cfa1288e35e3ceb1
Leonid <90864230+Leshev@users.noreply.github.com>
2025-07-09 23:50:57 +0500

    realpack.bat

--- realpack.bat
+++ realpack.bat
@@ -1,18 +1,18 @@
 chcp 65001
 echo off
-color 000A
-title Install plugin realpack 3.3
-mode con:cols=100 lines=11
+mode con:cols=20 lines=2
+title Install plugin realpack 3.4
 rmdir /S /q realpack
 md realpack
 cls
-echo Download realpack in GitHub by Leshev
+echo Download...
 powershell -Command "Invoke-WebRequest https://github.com/Leshev/realpack/raw/refs/heads/main/realpack.zip -OutFile realpack\package.zip"
 cls
-echo Instal files
+echo Install...
 powershell -command "Expand-Archive -Force '%~dp0realpack\package.zip' '%~dp0realpack\'"
 del realpack\package.zip
 cls
 echo Complited!
 timeout /t 2 >nul
 exit
+
