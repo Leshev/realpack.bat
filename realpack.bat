207496d87710bbf008ff1c26f9c5cf61531ebeb6
Leonid <90864230+Leshev@users.noreply.github.com>
2025-07-11 22:42:15 +0500

    realpack.bat

--- realpack.bat
+++ realpack.bat
@@ -1,18 +1,45 @@
 chcp 65001
 echo off
-mode con:cols=100 lines=20
-title Install plugin realpack 3.4
+title Install plugin realpack
 rmdir /S /q realpack
 md realpack
-cls
-echo Download...
+
 powershell -Command "Invoke-WebRequest https://github.com/Leshev/realpack/raw/refs/heads/main/realpack.zip -OutFile realpack\package.zip"
-cls
-echo Install...
 powershell -command "Expand-Archive -Force '%~dp0realpack\package.zip' '%~dp0realpack\'"
 del realpack\package.zip
+
+setlocal enabledelayedexpansion
+
+set "SOURCE_DIR=realpack"
+set "TARGET_DIR=%USERPROFILE%\AppData\Roaming\MCGL\MinecraftLauncher2\repository\mclient\plugins"
+set "TARGET_FOLDER=%TARGET_DIR%\realpack"
+
+if not exist "%SOURCE_DIR%" (
+ pause
+ exit /b
+)
+
+if not exist "%TARGET_DIR%" (
+ mkdir "%TARGET_DIR%"
+ if not exist "%TARGET_DIR%" (
+ pause
+ exit /b
+ )
+)
+
+if exist "%TARGET_FOLDER%" (
+  rd /s /q "%TARGET_FOLDER%"
+)
+
+move "%SOURCE_DIR%" "%TARGET_DIR%"
+
+if exist "%TARGET_FOLDER%" (
+ echo Папка realpack успешно перемещена в %TARGET_DIR%
+) else (
+ echo Ошибка при перемещении папки
+)
 cls
-echo Complited!
-timeout /t 2 >nul
-exit
+echo Успех!!! 
+echo Complited!!!
+timeout /t 3 >nul
 
